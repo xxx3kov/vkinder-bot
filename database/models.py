897c50f0bc48f84e4844702da15651748f988af7
xxx3kov <sanek.tretyakov565@gmail.com>
2026-03-13 19:26:51 +0300

    Initial project structure

---
+++ database/models.py
@@ -0,0 +1,17 @@
+"""
+Описание архитектуры БД (SQLAlchemy).
+Таблицы: User(кто пользуется ботом), DatingUser(кого нашли),
+UserView(избранные/пропущенные анкеты).
+"""
+
+
+class User:
+    pass
+
+
+class DatingUser:
+    pass
+
+
+class UserView:
+    pass
